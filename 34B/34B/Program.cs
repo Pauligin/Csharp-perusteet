56db7fbd146848181817bd9b37adac09f1ba52a2
Pauligin <pauli-kum@hotmail.com>
2023-04-17 19:57:32 +0300

    17.4Harjoitukset

---
+++ 34B/34B/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tee ohjelma, joka pyytää käyttäjältä sanan.
+            //Sovellus kopioi sanasta kaikki vokaalit taulukkoon. Lopuksi näytetään käyttäjälle sanan vokaalit.
+            //
+            //Päivän 7 harjoitukset mallina.
+
+            //Vokaalin voi tutkia esim: word[i] == 'a' || word[i] == 'e'
+
+            //EXTRA:
+
+            //Tee sovellus myös käyttämällä listaa.
+            
+            //1.Lue käyttäjältä sana talteen
+
+            Console.Write("Syötä sana: ");
+            string userInput = Console.ReadLine();
+
+
+
+            //2.Luo taulukko sanan vokaaleille
+
+            //Taulukon pituus täytyy määrittää, sitä luodessa
+            //Luodaan taulukon pituudeksi sama, kuin sanan pituut, silloin ainakin riittää tila
+
+            //datatyyppi [] taulukonNimi = new datatyyppi [taulukon pituus];
+            char[] vocalsInWorld = new char[userInput.Length]; //Taulukko
+
+            //List<datatyyppi> nimi = new List <datatyyppi>();
+            List<char> vocalsUsingList = new List<char>(); //Lista
+            //Lista on oletuksena tyhjä ja sen pituus/Count on 0
+            //vocalsUsingList[0] = 'n'; // Kaatuu, ArgumentOutOfRangeException, koska lista on tyhjä
+            //Sen elementtejä ei voida vielä käyttää
+
+
+            //3.Käy sanan merkit läpi silmukassa
+
+            //foreach olisi toinen vaihtoehto, mutta siinä on hankala saada indeksi, jota tarvitan taulukossa
+            //(int i = 0) indeksi alkaa arvosta 0, koska indeksit alkavat NOLLASTA, indeksi muuttujaa käytetään taulukossa
+            //Silmukan ehtona on taulukon pituus/ indeksien lukumäärä. Kun iteroitava muuttuja i
+            //on taulukon indeksien ulkopuolella, silmukka päättyy.
+            //Esim: "test".Length == 4, jonka indeksit ovat 0, 1, 2, 3. kun i saapuu arvoon 4, eli 4 < 4 ei ole enää totta silmukka päättyy
+            //iterointi, i++
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                //Kirjoitetaan merkin yksi kerrallaan, merkkiä voi käyttää syntaksilla
+                //muuttujanNimi[indeksi]
+                //Console.WriteLine(userInput[i]);
+
+                //4.Tallenna merkki taulukkoon, jos se on vokaali.
+                //Lisätään merkin tallennus ehdon sisälle, joka tarkistaa, että merkki on vokaali
+                if (userInput[i] == 'a' || userInput[i] == 'e' || userInput[i] == 'i' ||
+                    userInput[i] == 'o' || userInput[i] == 'u' || userInput[i] == 'y' ||
+                    userInput[i] == 'ä' || userInput[i] == 'ö')
+                {
+                    vocalsInWorld[i] = userInput[i];
+                    vocalsUsingList.Add(userInput[i]); //Listaan lisätään elementti. Add()
+                }
+
+            }
+
+
+            //5.Luo silmukka, jossa tulostetaan vokaalit taulukosta käyttäjälle.
+
+            //For olisi toinen vaihtoehto
+            foreach (char symbol in vocalsInWorld)// Taulukko
+            {
+                // Ei tulosteta tyhjiä elementtejä
+                // char taulokon oletusarvo on 0, jos symbol on muutakuin 0 (symbol != 0)
+                // Tulostetaan merkki
+                if (symbol != 0)
+                {
+                    Console.WriteLine(symbol);
+                }
+
+            }
+
+            //Myös foreach toimisi
+            for (int i = 0; i < vocalsUsingList.Count; i++) //Lista
+            {
+                Console.WriteLine("Lista: " + vocalsUsingList[i]);
+            }
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 37C/37C/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tee ohjelma, johon syötetään päivän lämpötilat.Lämpötilat mitataan kello 6, 12, 18 ja 24.
+            //Kun arvot on syötetty ohjelma laskee päivän suurimman, pienimmän ja keskilämpötilan.
+
+            //Tee sovellus samaan aikaan käyttämällä taulukkoa ja listaa.
+
+            //1.Luo taulukko ja lista arvoille.
+
+
+            string[] measurementTimes = { "6:00", "12:00", "18:00", "24:00" };
+
+            double[] temperaturesArray = new double[measurementTimes.Length];
+            List<double> temperaruesList = new List<double>(); //Lista
+
+            //2.Lue silmukka, jossa käyttäjän arvo luetaan ja tallennetaan taulukkoon ja listaan.
+            //(Arvo luetaan vain kerran per kierros ja se tallennetaan sekä taulukkoon ja listaan)
+
+            for (int i = 0; i < measurementTimes.Length; i++)
+            {
+                Console.Write($"Syötä kello {measurementTimes[i]} lämpötila: ");
+                double input = double.Parse(Console.ReadLine());
+                temperaturesArray[i] = input; //Taulukko
+                temperaruesList.Add(input); //lista
+            }
+
+            //3a.Ilmoita suurin, pienin ja keskilämpötila käyttämällä valmiita metodeja.
+            //4.Tulosta suurin, pienin ja keskilämpötila käyttäjälle.
+            Console.WriteLine($"Taulukon suurin lämpötila on: {temperaturesArray.Max()}");
+            Console.WriteLine($"Taulukon pienin lämpötila on: {temperaturesArray.Min()}");
+            Console.WriteLine($"Taulukon keskilämpötila on: {temperaturesArray.Average()}");
+
+            Console.WriteLine($"Taulukon suurin lämpötila on: {temperaturesArray.Max()}");
+            Console.WriteLine($"Taulukon pienin lämpötila on: {temperaturesArray.Min()}");
+            Console.WriteLine($"Taulukon keskilämpötila on: {temperaturesArray.Average()}");
+
+
+            //3b.Suorita arvojen etsiminen omalla koodilla.
+            //(Kysy esim ChatGPT: How to find the smallest value in an array using for loop)
+
+
+            //Algoritmi, termi koodille, joka suorittaa jonkin asian
+            //Tässä algoritmi, joka laskee keskiarvon
+
+            int count = 0;
+            double sum = 0;
+
+            foreach (double temp in temperaturesArray)
+            {
+                sum += temp;
+                count++;
+            }
+
+            Console.WriteLine($"Taulukon keskiarvo on: {sum / count}");
+
+            //Tässä algoritmi, joka hakee listasta suurimman arvon
+
+            double valueMin = 0; //Tästä arvosta lähdetään liikkeelle ja tallennetaan suurin arvo.
+
+            for (int i = 0; i < temperaruesList.Count; i++)
+            {
+
+                //Korvataan oletusarvo 0, ensimmäisellä kieroksella
+                if (i == 0)
+                {
+                    valueMin = temperaruesList[i];
+                }
+
+                //Onko uusi arvo pienempi kuin vanha arvo
+                if (temperaruesList[i] < valueMin)
+                {
+                    valueMin = temperaruesList[i]; // Otetaan talteen pienempi arvo
+                }
+            }
+
+            Console.WriteLine($"Listan pienin arvo on: {valueMin}");
+
+            //Tee algoritmi, joka hakee taulukosta pienimmän arvon
+
+            double value = 0; //Tästä arvosta lähdetään liikkeelle ja tallennetaan suurin arvo.
+
+            for (int i = 0; i < temperaruesList.Count; i++)
+            {
+
+                //Korvataan oletusarvo 0, ensimmäisellä kieroksella
+                if (i == 0)
+                {
+                    value = temperaruesList[i];
+                }
+
+                //Onko uusi arvo suurempi kuin vanha arvo
+                if (temperaruesList[i] > value)
+                {
+                    value = temperaruesList[i]; // Otetaan talteen isompi arvo
+                }
+            }
+
+            Console.WriteLine($"Listan suurin arvo on: {value}");
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
